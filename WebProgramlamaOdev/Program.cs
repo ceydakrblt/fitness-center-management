11c0ca4527a35f7812af88fbedf5fa5cfc0d6a26
ceydakrblt <ceydakarabulutt0@gmail.com>
2025-12-15 00:23:57 +0300

    AI ENTEGRASYONU VE İYİLEŞTİRMELER

--- WebProgramlamaOdev/Program.cs
+++ WebProgramlamaOdev/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 // ---------------------------------
-// IDENTITY
+
 // ---------------------------------
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
